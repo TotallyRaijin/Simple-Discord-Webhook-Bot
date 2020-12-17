12ac54d7fa8503accbc9661826b07a6a7ff148de
TheRealPikachu <therealpikachu008@gmail.com>
2020-12-17 11:35:01 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,11 +4,12 @@ var config = require('./config.json');
 const url = config.url
 const token = config.token
 const { Webhook } = require('discord-webhook-node');
+const hook = new Webhook(url);
 
 client.on('ready', () => {
   console.log(`I'm running!`);
 });
 
 client.on('message', msg => {
-  if (msg.content.startsWith === "!sendtowebhook") {
+  if (msg.content.startsWith('!sendtowebhook') {
   
